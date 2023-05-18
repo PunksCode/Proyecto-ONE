f1894a7aee0b795d6a90af04e9d93739a4872baa
PunksCode <agusxxpunk77@gmail.com>
2023-05-18 16:32:52 -0300

    Alert, y modal agregado con config de Css

--- script.js
+++ script.js
@@ -20,42 +20,69 @@ function btnDesencriptar(){
     
 }
 
-function encriptar(stringEncriptado){
-    let codigoMatriz = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringEncriptado=stringEncriptado.toLowerCase()
+function encriptar(stringEncriptado) {
+  let codigoMatriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  stringEncriptado = stringEncriptado.toLowerCase();
 
-    for (let i=0; i < codigoMatriz.length;i ++){
-        if(stringEncriptado.includes(codigoMatriz[i][0])){
-     stringEncriptado=stringEncriptado.replaceAll(codigoMatriz[i][0],codigoMatriz[i][1])
-        }
-    }
-    return stringEncriptado 
-}
-
-function desencriptar(stringDesencriptado){
-    let codigoMatriz = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringDesencriptado=stringDesencriptado.toLowerCase()
-
-    for (let i=0; i < codigoMatriz.length;i ++){
-        if(stringDesencriptado.includes(codigoMatriz[i][1])){
-            stringDesencriptado=stringDesencriptado.replaceAll(codigoMatriz[i][1],codigoMatriz[i][0])
-        }
-    }
-    return stringDesencriptado
-}
-function btnCopiar() {
-    const mensaje = document.querySelector(".mensaje");
- 
-    mensaje.select();
-  
-    // Copia el texto al portapapeles usando la API Clipboard
-    navigator.clipboard.writeText(mensaje.value)
-      .then(() => {
-        console.log("Texto copiado al portapapeles");
-      })
-      .catch((error) => {
-        console.error("Error al copiar el texto al portapapeles:", error);
-      });
+  if (stringEncriptado.trim() === "") {
+    alert("No hay texto a encriptar");
+    return stringEncriptado;
   }
 
+  for (let i = 0; i < codigoMatriz.length; i++) {
+    if (stringEncriptado.includes(codigoMatriz[i][0])) {
+      stringEncriptado = stringEncriptado.replaceAll(codigoMatriz[i][0], codigoMatriz[i][1]);
+    }
+  }
+  return stringEncriptado;
+}
 
+function desencriptar(stringDesencriptado) {
+  let codigoMatriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  stringDesencriptado = stringDesencriptado.toLowerCase();
+
+  if (stringDesencriptado.trim() === "") {
+    alert("No hay texto a desencriptar");
+    return stringDesencriptado;
+  }
+
+  for (let i = 0; i < codigoMatriz.length; i++) {
+    if (stringDesencriptado.includes(codigoMatriz[i][1])) {
+      stringDesencriptado = stringDesencriptado.replaceAll(codigoMatriz[i][1], codigoMatriz[i][0]);
+    }
+  }
+  return stringDesencriptado;
+}
+
+function btnCopiar() {
+  const mensaje = document.querySelector(".mensaje");
+
+  if (mensaje.value.trim() === "") {
+    // No hay texto para copiar
+    alert("No se ha encontrado texto para copiar");
+    return;
+  }
+
+  // Copiar el texto al portapapeles usando la API Clipboard
+  navigator.clipboard.writeText(mensaje.value)
+    .then(() => {
+      showSpam();
+    })
+    .catch((error) => {
+      console.error("Error al copiar el texto al portapapeles:", error);
+    });
+}
+
+function showSpam() {
+  const spamMessage = document.getElementById("spamMessage");
+  const modal = document.getElementById("modal");
+
+  spamMessage.textContent = "¡Texto copiado al portapapeles!";
+  modal.style.display = "block";
+  modal.classList.add("show-spam");
+
+  setTimeout(() => {
+    modal.style.display = "none";
+    modal.classList.remove("show-spam");
+  }, 2000); // Ocultar el mensaje después de 2 segundos
+}
